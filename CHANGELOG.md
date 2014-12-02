b48ca408cdf8b03affb64682658a12965e847073
Elliot Jordan <elliot@elliotjordan.com>
2014-12-02 06:30:10 -0800

    Added more settings verification, updated changelog and readme.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,10 +5,12 @@ Version 1.0 - 2014-11-24
 
 - Initial public release, with basic plugin counting and alerting built in.
 
-Version 1.1 - pending release
+Version 1.1 - 2014-12-02
 
 - Created change log (this file).
 - Added support for multiple WP sites on same host.
 - Added comments to settings section to explain the options.
 - Simplified logic to make the code easier to follow visually.
 - Added debug mode that lets you see the email that will be sent.
+- Added instructions for configuring multiple email recipients.
+- Verifies website and alert settings, and fails gracefully if they are set incorrectly.
--- wp_plugin_counter.sh
+++ wp_plugin_counter.sh
@@ -9,8 +9,8 @@
 #                   triggered hourly by cron.
 #          Author:  Elliot Jordan <elliot@elliotjordan.com>
 #         Created:  2014-11-20
-#   Last Modified:  2014-12-01
-#         Version:  1.1-beta
+#   Last Modified:  2014-12-02
+#         Version:  1.1
 #
 ###
 
@@ -59,7 +59,11 @@ SEND_SMS_ALERT_ON_CHANGE=false
 # If the above is true, specify your phone's email-to-txt address here.
 SMS_RECIPIENT="0005551212@txt.att.net"
 
-EMAIL_TO="you@pretendco.com"
+# The email notifications will be sent to this email address.
+# Multiple "to" addresses can be separated by a comma.
+EMAIL_TO="you@pretendco.com, somebodyelse@pretendco.com"
+
+# The email notifications will be sent from this email address.
 EMAIL_FROM="$(echo $(whoami)@$(hostname))"
 
 # Set to true if you'd like to receive an email regardless of the plugin count.
@@ -75,113 +79,131 @@ DEBUG_MODE=false
 
 
 # Let's make sure we have the same number of website settings.
-if [[ ${#WEBSITE_URL[@]} == ${#WEBSITE_ROOT[@]} &&
-      ${#WEBSITE_URL[@]} == ${#PLUGIN_DIR[@]} &&
-      ${#WEBSITE_URL[@]} == ${#LOG_FILE[@]} ]]; then
-
-    # Count the number of sites we need to process.
-    SITE_COUNT=${#WEBSITE_URL[@]}
-
-    # Begin main loop.
-    for (( i = 0; i < $SITE_COUNT; i++ )); do
-
-        # Get last plugin count and timestamp.
-        LAST_PLUGIN_COUNT="$(tail -n 1 "${LOG_FILE[$i]}" | awk -F' : | plugins.' {'print $2'})"
-        LAST_CHECK_DATE="$(tail -n 1 "${LOG_FILE[$i]}" | awk -F' : ' {'print $1'})"
-
-        # Get current plugin count and timestamp.
-        CURRENT_PLUGIN_COUNT="$(ls -l /"${WEBSITE_ROOT[$i]}/${PLUGIN_DIR[$i]}/" | grep -v " ./" | grep -v " ../" | grep -v "total " | grep -v " index.php" | wc -l)"
-        CURRENT_CHECK_DATE="$(date)"
-
-        # Compare current count to last count and return result.
-        if [[ "$LAST_PLUGIN_COUNT" != "$CURRENT_PLUGIN_COUNT" ]]; then
-            HAS_COUNT_CHANGED=true
-        else
-            HAS_COUNT_CHANGED=false
-        fi
-
-        # Write a new count and timestamp to the logs.
-        echo "$CURRENT_CHECK_DATE : $CURRENT_PLUGIN_COUNT plugins." >> ${LOG_FILE[$i]}
-
-        # If the count has changed, send an email.
-        if [[ $HAS_COUNT_CHANGED == true ]]; then
-
-            if [[ $SEND_SMS_ALERT_ON_CHANGE == true && $SMS_RECIPIENT != "0005551212@txt.att.net" ]]; then
-
-                SMS_MESSAGE="Plugin alert for ${WEBSITE_URL[$i]}. Details sent to $EMAIL_TO.\n.\n"
-                printf "$SMS_MESSAGE" | /usr/sbin/sendmail "$SMS_RECIPIENT"
-
-            fi
-
-            EMAIL_SUBJ="[${WEBSITE_URL[$i]}] WordPress plugin count change detected"
-            EMAIL_MSG="WARNING: The number of WordPress plugins on ${WEBSITE_URL[$i]} has changed since our last check:\n\n"
-
-            # Include last two lines from the log.
-            EMAIL_MSG+="$(tail -n 2 "${LOG_FILE[$i]}")\n\n"
-
-            # Include git status, if that option is enabled.
-            if [[ $INCLUDE_GIT_STATUS == true ]]; then
-                GIT_STATUS="$(cd "${WEBSITE_ROOT[$i]}"; git status)"
-                if [[ $? == 0 ]]; then
-                    EMAIL_MSG+="Here are the file-level changes, as reported by Git:\n\n$GIT_STATUS\n\n"
-                else
-                    EMAIL_MSG+="(An error occurred while trying to check Git status.)\n\n"
-                fi
-            fi
-
-            EMAIL_MSG+="Thank you."
-
-            # Construct the message.
-            SENDMAIL="From: $EMAIL_FROM\nTo: $EMAIL_TO\nSubject: $EMAIL_SUBJ\n$EMAIL_MSG\n.\n"
-
-            if [[ $DEBUG_MODE == true ]]; then
-                # Print the message, if in debug mode.
-                printf "$SENDMAIL"
-            else
-                # Send the message.
-                printf "$SENDMAIL" | /usr/sbin/sendmail "$EMAIL_TO"
-            fi
-
-        elif [[ $SEND_ALERTS_WHEN_COUNT_UNCHANGED == true ]]; then
-
-            EMAIL_SUBJ="[${WEBSITE_URL[$i]}] WordPress plugin count verified"
-            EMAIL_MSG="WARNING: The number of WordPress plugins on ${WEBSITE_URL[$i]} has not changed since our last check:\n\n"
-
-            # Include last line from the log.
-            EMAIL_MSG+="$(tail -n 1 "${LOG_FILE[$i]}")\n\n"
-
-            # Include git status, if that option is enabled.
-            if [[ $INCLUDE_GIT_STATUS == true ]]; then
-                GIT_STATUS="$(cd "${WEBSITE_ROOT[$i]}"; git status)"
-                if [[ $? == 0 ]]; then
-                    EMAIL_MSG+="Here are the file-level changes, as reported by Git:\n\n$GIT_STATUS\n\n"
-                else
-                    EMAIL_MSG+="An error occurred while trying to check Git status:\n\n$GIT_STATUS\n\n"
-                fi
-            fi
-
-            EMAIL_MSG+="Thank you."
-
-            # Construct the message.
-            SENDMAIL="From: $EMAIL_FROM\nTo: $EMAIL_TO\nSubject: $EMAIL_SUBJ\n$EMAIL_MSG\n.\n"
-
-            if [[ $DEBUG_MODE == true ]]; then
-                # Print the message, if in debug mode.
-                printf "$SENDMAIL"
-            else
-                # Send the message.
-                printf "$SENDMAIL" | /usr/sbin/sendmail "$EMAIL_TO"
-            fi
-
-        fi
-
-    done
-    # End main loop.
-
-else
+if [[ ${#WEBSITE_URL[@]} != ${#WEBSITE_ROOT[@]} ||
+      ${#WEBSITE_URL[@]} != ${#PLUGIN_DIR[@]} ||
+      ${#WEBSITE_URL[@]} != ${#LOG_FILE[@]} ]]; then
 
     echo "Error: Please carefully check the website settings in the wp_plugin_counter.sh file. The number of parameters don't match."
 
-fi
+else # Website settings verified.
+
+    # Let's make sure we aren't using default email alert settings.
+    if [[ $EMAIL_TO == "you@pretendco.com, somebodyelse@pretendco.com" ]]; then
+
+        echo "Error: The email alert settings are still using their default email and SMS addresses. Please edit them to suit your environment."
+
+    else # Email alert settings verified.
+
+        # Let's make sure we aren't using default SMS alert settings.
+        if [[ $SEND_SMS_ALERT_ON_CHANGE == true &&
+              $SMS_RECIPIENT == "0005551212@txt.att.net" ]]; then
+
+            echo "Error: The SMS alert settings are still using their default email and SMS addresses. Please edit them to suit your environment."
+
+        else # SMS alert settings verified.
+
+            # Count the number of sites we need to process.
+            SITE_COUNT=${#WEBSITE_URL[@]}
+
+            # Begin main loop.
+            for (( i = 0; i < $SITE_COUNT; i++ )); do
+
+                # Get last plugin count and timestamp.
+                LAST_PLUGIN_COUNT="$(tail -n 1 "${LOG_FILE[$i]}" | awk -F' : | plugins.' {'print $2'})"
+                LAST_CHECK_DATE="$(tail -n 1 "${LOG_FILE[$i]}" | awk -F' : ' {'print $1'})"
+
+                # Get current plugin count and timestamp.
+                CURRENT_PLUGIN_COUNT="$(ls -l /"${WEBSITE_ROOT[$i]}/${PLUGIN_DIR[$i]}/" | grep -v " ./" | grep -v " ../" | grep -v "total " | grep -v " index.php" | wc -l)"
+                CURRENT_CHECK_DATE="$(date)"
+
+                # Compare current count to last count and return result.
+                if [[ "$LAST_PLUGIN_COUNT" != "$CURRENT_PLUGIN_COUNT" ]]; then
+                    HAS_COUNT_CHANGED=true
+                else
+                    HAS_COUNT_CHANGED=false
+                fi
+
+                # Write a new count and timestamp to the logs.
+                echo "$CURRENT_CHECK_DATE : $CURRENT_PLUGIN_COUNT plugins." >> ${LOG_FILE[$i]}
+
+                # If the count has changed, send an email.
+                if [[ $HAS_COUNT_CHANGED == true ]]; then
+
+                    if [[ $SEND_SMS_ALERT_ON_CHANGE == true && $SMS_RECIPIENT != "0005551212@txt.att.net" ]]; then
+
+                        SMS_MESSAGE="Plugin alert for ${WEBSITE_URL[$i]}. Details sent to $EMAIL_TO.\n.\n"
+                        printf "$SMS_MESSAGE" | /usr/sbin/sendmail "$SMS_RECIPIENT"
+
+                    fi
+
+                    EMAIL_SUBJ="[${WEBSITE_URL[$i]}] WordPress plugin count change detected"
+                    EMAIL_MSG="WARNING: The number of WordPress plugins on ${WEBSITE_URL[$i]} has changed since our last check:\n\n"
+
+                    # Include last two lines from the log.
+                    EMAIL_MSG+="$(tail -n 2 "${LOG_FILE[$i]}")\n\n"
+
+                    # Include git status, if that option is enabled.
+                    if [[ $INCLUDE_GIT_STATUS == true ]]; then
+                        GIT_STATUS="$(cd "${WEBSITE_ROOT[$i]}"; git status)"
+                        if [[ $? == 0 ]]; then
+                            EMAIL_MSG+="Here are the file-level changes, as reported by Git:\n\n$GIT_STATUS\n\n"
+                        else
+                            EMAIL_MSG+="(An error occurred while trying to check Git status.)\n\n"
+                        fi
+                    fi
+
+                    EMAIL_MSG+="Thank you."
+
+                    # Construct the message.
+                    SENDMAIL="From: $EMAIL_FROM\nTo: $EMAIL_TO\nSubject: $EMAIL_SUBJ\n$EMAIL_MSG\n.\n"
+
+                    if [[ $DEBUG_MODE == true ]]; then
+                        # Print the message, if in debug mode.
+                        printf "$SENDMAIL"
+                    else
+                        # Send the message.
+                        printf "$SENDMAIL" | /usr/sbin/sendmail "$EMAIL_TO"
+                    fi
+
+                elif [[ $SEND_ALERTS_WHEN_COUNT_UNCHANGED == true ]]; then
+
+                    EMAIL_SUBJ="[${WEBSITE_URL[$i]}] WordPress plugin count verified"
+                    EMAIL_MSG="WARNING: The number of WordPress plugins on ${WEBSITE_URL[$i]} has not changed since our last check:\n\n"
+
+                    # Include last line from the log.
+                    EMAIL_MSG+="$(tail -n 1 "${LOG_FILE[$i]}")\n\n"
+
+                    # Include git status, if that option is enabled.
+                    if [[ $INCLUDE_GIT_STATUS == true ]]; then
+                        GIT_STATUS="$(cd "${WEBSITE_ROOT[$i]}"; git status)"
+                        if [[ $? == 0 ]]; then
+                            EMAIL_MSG+="Here are the file-level changes, as reported by Git:\n\n$GIT_STATUS\n\n"
+                        else
+                            EMAIL_MSG+="An error occurred while trying to check Git status:\n\n$GIT_STATUS\n\n"
+                        fi
+                    fi
+
+                    EMAIL_MSG+="Thank you."
+
+                    # Construct the message.
+                    SENDMAIL="From: $EMAIL_FROM\nTo: $EMAIL_TO\nSubject: $EMAIL_SUBJ\n$EMAIL_MSG\n.\n"
+
+                    if [[ $DEBUG_MODE == true ]]; then
+                        # Print the message, if in debug mode.
+                        printf "$SENDMAIL"
+                    else
+                        # Send the message.
+                        printf "$SENDMAIL" | /usr/sbin/sendmail "$EMAIL_TO"
+                    fi
+
+                fi
+
+            done # End main loop.
+
+        fi # End default SMS alert settings verification.
+
+    fi # End default email alert settings verification.
+
+fi # End website settings verification.
 
 exit $?
